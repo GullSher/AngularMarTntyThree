1e7fa701b665dc4cdcfc04879423b19c335634b2
Gull-Sher <mobilegull73@gmail.com>
2023-03-16 01:51:41 +0500

    aaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'AngularMarTntyThree';
+  title = 'AngularMarTntyThree' + '  Here i want to add white space    ' + ' This is written joining two lines. ';
 }
