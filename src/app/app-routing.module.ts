a00e35d59c20cec8fa678e8a5d29859993a11e16
Gull-Sher <mobilegull73@gmail.com>
2023-03-17 01:41:46 +0500

    abc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pg1Component } from './pg1/pg1.component';
+import { Pg2Component } from './pg2/pg2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: Pg1Component,
+    path: 'pg1'
+  }, {
+    component: Pg2Component,
+    path: 'pg2'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
