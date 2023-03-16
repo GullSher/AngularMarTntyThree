cffacc7f540c8e08fb8cf715cc61cbc0f120c64c
Gull-Sher <mobilegull73@gmail.com>
2023-03-17 01:00:22 +0500

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { Pg1Component } from './pg1/pg1.component';
+import { Pg2Component } from './pg2/pg2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    Pg1Component,
+    Pg2Component
   ],
   imports: [
     BrowserModule,
